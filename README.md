407dcb7ee51bf54482911a7f23a08a1721f2ff8f
pererawdps <pererawdps@gmail.com>
2021-09-08 07:12:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xx29o2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xx29o2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('bindingInput') bindingInput!: ElementRef;
+
+  isUnchanged = true;
+
+  getHTMLAttributeValue(): any {
+    console.warn('HTML attribute value: ' + this.bindingInput.nativeElement.getAttribute('value'));
+  }
+
+  getDOMPropertyValue(): any {
+    console.warn('DOM property value: ' + this.bindingInput.nativeElement.value);
+  }
+
+  working(): any {
+    console.warn('Test Button works!');
+  }
+
+  toggleDisabled(): any {
+
+    const testButton = document.getElementById('testButton') as HTMLInputElement;
+    testButton.disabled = !testButton.disabled;
+    console.warn(testButton.disabled);
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
